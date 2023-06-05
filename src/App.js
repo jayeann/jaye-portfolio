64a1a30eca5f20b0daed5b95ba45e2b6caddcc37
jayeann <jgtagoctoc@gmail.com>
2023-06-05 14:56:32 +0800

    modify import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 
 //common
 import NavigationBar from "./common/NavigationBar";
